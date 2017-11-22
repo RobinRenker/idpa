1ef4f8c609b29bb8bea497db4352246a3eb9d30e
Volken Jonas ( U115745 ) <jonas.volken@mobi.ch>
2017-11-22 13:09:38 +0100

    function

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,22 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate(function (event) {
+    var uid = event.data.uid;
+    var displayName = event.data.displayName;
+
+
+
+    admin.firestore().collection("users").add({
+        first: "HURE TEST",
+        last: "SOHN",
+        uid: uid,
+        username: displayName
+    });
+
+
+});
